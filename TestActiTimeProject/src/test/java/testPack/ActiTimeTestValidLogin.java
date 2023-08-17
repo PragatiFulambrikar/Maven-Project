dc44e9c25f4f976f9e9516bb32041cbf61e1d67d
Admin <Admin@DESKTOP-23DKO3H>
2023-08-17 20:54:29 +0530

    commited!

---
+++ TestActiTimeProject/src/test/java/testPack/ActiTimeTestValidLogin.java
@@ -0,0 +1,37 @@
+package testPack;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class ActiTimeTestValidLogin {
+	@Test
+	public void method1()
+	{
+ 	WebDriverManager.chromedriver().setup();
+	WebDriver driver= new ChromeDriver();
+	 driver.manage().window().maximize();
+	 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	 driver.get("http://desktop-23dko3h/login.do");
+	 driver.findElement(By.name("username")).sendKeys("admin");
+	 driver.findElement(By.name("pwd")).sendKeys("manager"); 
+	 driver.findElement(By.id("loginButton")).click(); 
+	 Reporter.log("valid login",true);
+	}
+	@Test
+	public void method2()
+	{
+	 WebDriverManager.chromedriver().setup();
+	 //System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+	 WebDriver driver= new ChromeDriver();
+	 driver.manage().window().maximize();
+	 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	 driver.get("http://desktop-23dko3h/login.do");
+	 driver.findElement(By.name("username")).sendKeys("admin1");
+	 driver.findElement(By.name("pwd")).sendKeys("manager1"); 
+	 driver.findElement(By.id("loginButton")).click(); 
+	 Reporter.log("invalid login",true);
+	}
+}
